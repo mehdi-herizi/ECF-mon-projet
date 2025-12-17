781f982d686e378fe9fb1c7db9adfbd19c951d6c
Mehdi <mehdih13014@gmail.com>
2025-12-17 21:55:07 +0100

    17/12/2025 avancement du projet

--- nexary.js
+++ nexary.js
@@ -1,30 +1,54 @@
- console.log("hello")
- 
- let jeux = [];
-
 const gameList = document.getElementById("categorie");
 
-for (let i = 0; i < jeux.length; i++) {
+async function chargerJSON() {
+  try {
+    const response = await fetch("gameList.json");
 
-    const monthDiv = document.createElement("div");
-    monthDiv.classList.add("month");
-    
-    const h3 = document.createElement("h3");
-    h3.innerText = jeux[i].month;
-    
-    const jeuxTitre = document.createElement("p");
-    jeuxTitre.innerText = "🎮 " + jeux[i].game;
-    jeuxTitre.style.fontWeight = "bold";
-    
-    const description = document.createElement("p");
-    description.innerText = jeux[i].description;
+    if (!response.ok) {
+      throw new Error("Problème de chargement");
+    }
 
-    const ImageJeux = document.createElement("img")
-    ImageJeux.src = "image/StarForge.png"
-    document.getElementById
-    monthDiv.appendChild(h3);
-    monthDiv.appendChild(jeuxTitre);
-    monthDiv.appendChild(description);
-    
-    gameList.appendChild(monthDiv);
+    const jeux = await response.json();
+    console.log("JEUX :", jeux);
+
+    jeux.forEach((jeu) => {
+      const image = document.createElement("img");
+      image.src = jeu.imageUrl;
+
+      const monthDiv = document.createElement("div");
+      monthDiv.classList.add("month");
+
+      const h3 = document.createElement("h3");
+      h3.innerText = jeu.name;
+
+      const date = document.createElement("p");
+      date.innerText = jeu.date;
+
+      const categorie = document.createElement("p");
+      categorie.innerText = "🎮 " + jeu.categorie;
+      categorie.style.fontWeight = "bold";
+
+      const description = document.createElement("p");
+      description.innerText = jeu.description;
+
+      const MaDivReaseaux = document.createElement("div");
+      MaDivReaseaux.classList = ""
+      
+      jeu.reesauxSociaux.forEach((reseau) => {});
+
+      monthDiv.appendChild(image);
+      monthDiv.appendChild(h3);
+      monthDiv.appendChild(date);
+      monthDiv.appendChild(categorie);
+      monthDiv.appendChild(imagereseaux);
+      monthDiv.appendChild(description);
+
+      gameList.appendChild(monthDiv);
+    });
+  } catch (error) {
+    console.error(error);
+  }
 }
+
+chargerJSON();
+//TODO faut que je mette categorie dans un tableau
